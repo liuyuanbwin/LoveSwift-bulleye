6c09a122a1c446f32d066827afea67e3980822a4
LeoLau <liuyuansix@163.com>
2018-03-05 14:58:40 +0800

    完成了 UIAlertController 的 Demo

--- LoveSwift-BullEye/ViewController.swift
+++ LoveSwift-BullEye/ViewController.swift
@@ -19,7 +19,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func showAlert(){
+        let alert = UIAlertController(title: "Hello Russia", message: "梅西上演帽子戏法", preferredStyle: .alert)
+        let action = UIAlertAction(title: "确认", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
